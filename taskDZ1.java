7126bce764940179f97a6420bdbb99c99756d9b1
mexatron <mexatron>
2023-03-04 23:39:28 +0200

    DZ1 done

---
+++ taskDZ1.java
@@ -0,0 +1,37 @@
+/* Вычислить n-ое треугольного число(сумма чисел от 1 до n), а так же n! (произведение чисел от 1 до n)
+Ввод:5
+Треугольное число 1 + 2 + 3 + 4 + 5 = 15
+n! 1 * 2 * 3 * 4 * 5 = 120 */
+
+import java.util.Scanner;
+
+public class taskDZ1 {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in, "Cp866");
+        System.out.print("Enter number: ");
+   
+        int num = iScanner.nextInt();
+        while(num<1){
+            System.out.println("Enter number more then 0");
+            num = iScanner.nextInt();
+        }
+        int i = 2;
+        int sum = 1;
+        int n = 1;
+        System.out.print("Треугольное число 1");
+        while (i <= num) {
+            sum += i;
+            System.out.printf(" + %d", i);        
+            i++;
+        }
+        System.out.printf(" = %d\n", sum);
+        i=1;
+        System.out.print("n1 1");
+        while (i <= num) {
+            n*=i;
+            System.out.printf(" * %d", n);        
+            i++;
+        }
+        System.out.printf(" = %d\n", n);
+    }
+}
