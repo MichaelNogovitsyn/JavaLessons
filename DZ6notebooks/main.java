3cc943d25b19e4e8b35347f495ac2fc9ffcfceda
Michael <mechatron86@gmail.com>
2023-03-22 17:58:01 +0200

    do do it

--- DZ6notebooks/main.java
+++ DZ6notebooks/main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /* Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
 Создать множество ноутбуков.
 Написать метод, который будет запрашивать у пользователя критерий фильтрации и выведет ноутбуки, отвечающие фильтру.
@@ -26,6 +28,59 @@ NoteBook notebook5 = new NoteBook
 
 public class main {
     public static void main(String[] args) {
-        
+        Scanner scn = new Scanner(System.in);
+        Notebook notebook1 = new Notebook();
+        notebook1.addModel("Asus");
+        notebook1.addColor("White");
+        notebook1.addOS("Windows 10");
+        notebook1.addRAM(8);
+        notebook1.addVD(120);
+
+        Notebook notebook2 = new Notebook();
+        notebook2.model = "Razer";
+        notebook2.color = "Black";
+        notebook2.os = "MacOS";
+        notebook2.ram = 16;
+        notebook2.vd = 256;
+
+        Notebook notebook3 = new Notebook();
+        notebook3.model = "Intel";
+        notebook3.color = "Red";
+        notebook3.os = "MacOS";
+        notebook3.ram = 32;
+        notebook3.vd = 512;
+
+        Notebook notebook4 = new Notebook();
+        notebook4.model = "AMD";
+        notebook4.color = "Blue";
+        notebook4.os = "Windows 11";
+        notebook4.ram = 8;
+        notebook4.vd = 512;
+
+        Notebook notebook5 = new Notebook();
+        notebook5.model = "Lenovo";
+        notebook5.color = "White";
+        notebook5.os = "Windows 11";
+        notebook5.ram = 16;
+        notebook5.vd = 1024;
+
+        int key = 0;
+        System.out.println("Здравствуйте! Введите цифру, соответствующую необходимому критерию: \n1 - ОЗУ\n2 - Объем ЖД\n3 - Операционная система\n4 - Цвет");         
+        switch (key) {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            default:
+                break;
+        }
     }
 }
--- DZ6notebooks/notebook.java
+++ DZ6notebooks/notebook.java
@@ -1,5 +1,55 @@
-public class notebook {
-    public static void main(String[] args) {
-        
+import javax.naming.spi.DirStateFactory.Result;
+
+
+public class Notebook {
+
+    int ram = 1;
+    int vd = 1;
+    String os = "no os";
+    String color = " ";
+    String model="noname";
+
+    @Override
+    public String toString() {
+        // String description = "";
+        return model + ", " + os + ", " + color + ", " + ram + "GB" +", " + vd+"GB.";
     }
+
+    public String getOs() {
+        return os;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public int getVd() {
+        return vd;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void addModel(String name) {
+        model = name;
+    }
+
+    public void addColor(String colr) {
+        color = colr;
+    }
+    public void addOS(String name) {
+        os = name;
+    }
+    public void addRAM(int name) {
+        ram = name;
+    }
+    public void addVD(int name) {
+        vd= name;
+    }
+
 }
