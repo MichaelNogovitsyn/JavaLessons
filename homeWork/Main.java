89dcca7a068e4b85aaf22e6c51f5e7e5ca7049d4
Michael <mechatron86@gmail.com>
2023-03-30 17:47:02 +0200

    homework2 done add Tolstoy file

--- homeWork/Main.java
+++ homeWork/Main.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.io.*;
-
 public class Main implements Serializable {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
@@ -81,33 +80,25 @@ public class Main implements Serializable {
                     break;
             }
         }
-
-        /*
-         * Member member1 = new Member(1685, 1728, "Иван Петрович Толстой", Gender.Male,
-         * null, null);
-         * Member member2 = new Member(1693, 1748, "Прасковья Михайловна Ртищева",
-         * Gender.Female, null, null);
-         * Member member3 = new Member(1727, 1811, "Александра Ивановна Щетинина",
-         * Gender.Female, member1, member2);
-         * Member member4 = new Member(1721, 1803, "Андрей Иванович Толстой",
-         * Gender.Male, member1, member2);
-         * Member member5 = new Member(1724, 1811, "Николай Иванович Горчаков",
-         * Gender.Male, member1, member2);
-         * Member member6 = new Member(1757, 1820, "Прасковья Васильевна Толстая",
-         * Gender.Female, null, null);
-         * Member member7 = new Member(1757, 1820, "Илья Андреевич Толстой",
-         * Gender.Male, member4, null);
-         * Member member8 = new Member(1757, 1820, "Полагея Николаевна Горчакова",
-         * Gender.Female, member5, null);
-         */
-
-        // List<Member> members = new ArrayList(Arrays.asList(member1, member2, member3,
-        // member4, member5, member6, member7, member8));
-        familyTolstoy.addAllMember(family);
-
+     
         // System.out.println("Члены семьи: " + familyTolstoy);
 
         // System.out.println("Дети :" + member1.getChildren());
-
     }
 }
+/*         Person member1 = new Person (1685, 1728, "Иван Петрович Толстой", Gender.Male,null, null);
+        Person  member2 = new Person (1693, 1748, "Прасковья Михайловна Ртищева",
+         Gender.Female, null, null);
+         Person  member3 = new Person (1727, 1811, "Александра Ивановна Щетинина",
+         Gender.Female, member1, member2);
+         Person  member4 = new Person (1721, 1803, "Андрей Иванович Толстой",
+         Gender.Male, member1, member2);
+         Person  member5 = new Person (1724, 1811, "Николай Иванович Горчаков",
+         Gender.Male, member1, member2);
+         Person  member6 = new Person (1757, 1820, "Прасковья Васильевна Толстая",
+         Gender.Female, null, null);
+         Person  member7 = new Person (1757, 1820, "Илья Андреевич Толстой",
+         Gender.Male, member4, null);
+         Person  member8 = new Person (1757, 1820, "Полагея Николаевна Горчакова",
+         Gender.Female, member5, null); */
+         //familyTolstoy.addAllMember(family);
